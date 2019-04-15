b5fde83fca0fd938be45274116d3f8d89f5dd22a
dadia1 <47502747+dadia1@users.noreply.github.com>
2019-04-15 23:38:18 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusinessManagementSoftware
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new BMSoftware());
+        }
+
+        public static int counterIncoms = 1;
+        public static int counterExpenss = 1;
+
+    }
+}
