abd6de140f71efc44a02da035322d14d41b70d56
dadia1 <47502747+dadia1@users.noreply.github.com>
2019-04-15 23:36:59 +0300

    Create Expenses.cs

---
+++ Expenses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessManagementSoftware
+{
+    class Expenses
+    {
+        public string _dateTimePickerExpenss;
+        public double expensessAmount;
+        public string txtDescripitionExpense;
+        public double refundExpenses;
+    }
+}
