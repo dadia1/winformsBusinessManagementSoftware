a8473f04bef4a79a606eedd46f69d1bd89454a18
dadia1 <47502747+dadia1@users.noreply.github.com>
2019-04-15 23:37:02 +0300

    Create Incomes.cs

---
+++ Incomes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessManagementSoftware
+{
+    class Incomes
+    {
+        public string _dateTimePickerIncomes;
+        public double incomesAmount;
+        public string txtDescripitionIcomes;
+    }
+}
